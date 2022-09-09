a4ebc77a80360147d0aa1f8b8ee974d3f6814cbb
billymwangi <billy.mwangi@student.moringaschool.com>
2022-09-09 12:01:52 +0300

    initial commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,51 @@ class ApplicationController < Sinatra::Base
   get "/" do
     { message: "Good luck with your project!" }.to_json
   end
+  get "/expenses" do
+    expenses = Expense.all 
+    expenses.to_json 
+  end
 
+  get "/expenses/:id" do
+    expenses = Expense.find(params[:id])
+    expenses.to_json 
+  end
+
+  post "/expenses" do 
+      expense = Expense.create(
+        name: params[:name],
+        cost: params[:cost] 
+      )
+      expense.to_json
+  end
+
+  put "/expense/:id" do
+      expense = Expense.find(params[:id])
+      expense.update(name: params[:name])
+      expense.update(cost: params[:cost])
+      expense.to_json
+  end
+
+  delete "/expenses/:id" do
+        expense = Expense.find(params[:id])
+        expense.destroy
+        expense.to_json
+  end
+
+  get "/categories/:id" do
+    categories = Category.find(params[:id])
+    categories.to_json 
+  end
+  
+  post "/categories" do 
+    category = Category.create(
+      name: params[:name], 
+    )
+    category.to_json
+  end
+
+  get "/categories" do
+    categories = Category.all 
+    categories.to_json 
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,10 @@
 puts "🌱 Seeding spices..."
 
 # Seed your database here
+Expense.create(name: "barber", cost: 250)
+Expense.create(name: "books", cost: 1000)
+Expense.create(name: "grocery shopping", cost: 2000)
 
+Category.create(name:"Utilities")
+Category.create(name:"Clothing")
 puts "✅ Done seeding!"
